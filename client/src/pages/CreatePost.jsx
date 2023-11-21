a8a6b0b518ac9263f31874d7fe70daea01a1efaf
Aviral Srivastava <kailwensen@gmail.com>
2023-11-21 21:39:50 +0530

    homepage structure ready

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  const navigate = useNavigate();
+  const [form, setform] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
+  const handleChange = (e) => {};
+  const handleSurpriseMe = (e) => {};
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
+          Create imaginitive and visually stunning images created by DALL-E AI
+        </p>
+      </div>
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col hap-5">
+          <FormField
+            lableName="Your Name"
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            lableName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="A Samurai riding a Horse on Mars, lomography"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default CreatePost;
